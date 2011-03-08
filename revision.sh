ba8914f33caad7550781e8a33ce0f9c42bacf0cf
Oliver Jakoubek <mail@oliverjakoubek.de>
2011-03-08 14:37:31 +0100

    Versionierung
    Das Skript revision.sh schreibt jetzt einen per Kommandozeile zu
    übergebenden Versionsstring, das aktuelle Datum und die aktuelle
    (letzte) Git-Revision in die Datei conf/messages.xx
    
    	modified:   conf/messages.xx
        modified:   revision.sh

--- revision.sh
+++ revision.sh
@@ -1,1 +1,9 @@
-git log --abbrev-commit -1 --pretty=%h > app/views/tags/revision.html
+file=conf/messages.xx
+
+version=$1
+releasedate=$(date +%Y-%m-%d)
+rev=`git log --abbrev-commit -1 --pretty=%h`
+
+echo "version=$version" > $file
+echo "releasedate=$releasedate" >> $file
+echo "revision=$rev" >> $file
